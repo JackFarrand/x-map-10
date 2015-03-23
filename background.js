0a3b806136ecd9adf19e476fc8971cf541262fa6
Jack <jack.farrand@gmail.com>
2015-03-23 21:20:12 +0000

    cleaned up indendation on all files, removed default google maps controls, including street view, centered the map's starting point on london

--- background.js
+++ background.js
@@ -4,74 +4,74 @@ var lat, lon, heading;			//Storage for lattitude, longitude, and headinging of t
 
 
 chrome.app.runtime.onLaunched.addListener(
-											function() 
-											{
-													  chrome.app.window.create('window.html', 
-													  {
-														id: "mainwin",
-														innerBounds: {width: 500, height: 309}
-													  }, 
-													  
-													  function(win) //callback
-													  {
-													  mainWindowID = win.id;
-													  }
-																			);   
-											});
+  function() 
+  {
+    chrome.app.window.create('window.html', 
+			     {
+				id: "mainwin",
+				innerBounds: {width: 500, height: 309}
+			     }, 
+			     
+			     function(win) //callback
+			     {
+			       mainWindowID = win.id;
+			     }
+    );   
+  });
 
 
 // Create the Socket
 chrome.sockets.udp.create({}, 	function(socketInfo) 
-								{
-									socketId = socketInfo.socketId;
-										  
-									// Setup event handler and bind socket.
-									chrome.sockets.udp.onReceive.addListener(onReceive);
-									chrome.sockets.udp.bind(socketId, "0.0.0.0", 49003, function(result) 
-																						{
-																							if (result < 0) 
-																							{
-																								console.log("Error binding socket.");
-																								return;
-																							}
-																							else
-																								console.log("socket bound!");     
-																						}
-															);
-								}
-						);
+{
+  socketId = socketInfo.socketId;
+  
+  // Setup event handler and bind socket.
+  chrome.sockets.udp.onReceive.addListener(onReceive);
+  chrome.sockets.udp.bind(socketId, "0.0.0.0", 49003, function(result) 
+  {
+    if (result < 0) 
+    {
+      console.log("Error binding socket.");
+      return;
+    }
+    else
+      console.log("socket bound!");     
+  }
+  );
+}
+);
 
 var onReceive = function(info) // Handle the Socket "onReceive" event.
 {
-	if (info.socketId !== socketId)
-		return;
-	  
-	var recLen = info.data.byteLength;
-
-	recLen -= 36; //subtract the byte length of a data set transmission from XP10 from the Bytes received, to make sure we don't indexOutOfBounds
-
-	for(var i = 5; i <= recLen; i+=36)
-	{
-		//first 4 bytes are the integer that identifies the next 8 floats.
-		var id = new Uint32Array(info.data.slice(i, i+4))[0];  //console.log("identifier received: " + id);
-			
-		if(id === 20) //ID for Lattitude and Longitude Etc.
-		{     
-			var floatView = new Float32Array(info.data.slice(i+4, i+36));    
-		
-			lat = floatView[0];
-			lon = floatView[1];
-		}
-		
-		if(id === 17)
-		{     
-		   var floatView = new Float32Array(info.data.slice(i+4, i+36));    
-		
-		   heading = floatView[2];
-		}
-	}
-
-	if(chrome.app.window.get("mainwin")) //if the damn window exists, write a message to it, else, just return.
-		chrome.app.window.get("mainwin").contentWindow.document.getElementById("mapFrame").contentWindow.postMessage("" + lat + "," + lon + "," + heading, '*');
-	  
+  if (info.socketId !== socketId)
+    return;
+  
+  var recLen = info.data.byteLength;
+  
+  recLen -= 36; //subtract the byte length of a data set transmission from XP10 from the Bytes received, to make sure we don't indexOutOfBounds
+  
+  for(var i = 5; i <= recLen; i+=36)
+  {
+    //first 4 bytes are the integer that identifies the next 8 floats.
+    var id = new Uint32Array(info.data.slice(i, i+4))[0];  //console.log("identifier received: " + id);
+    
+    if(id === 20) //ID for Lattitude and Longitude Etc.
+    {     
+      var floatView = new Float32Array(info.data.slice(i+4, i+36));    
+      
+      lat = floatView[0];
+      lon = floatView[1];
+    }
+    
+    if(id === 17)
+    {     
+      var floatView = new Float32Array(info.data.slice(i+4, i+36));    
+      
+      heading = floatView[2];
+    }
+  }
+  
+  if(chrome.app.window.get("mainwin")) //if the damn window exists, write a message to it, else, just return.
+    chrome.app.window.get("mainwin").contentWindow.document.getElementById("mapFrame").contentWindow.postMessage("" + lat + "," + lon + "," + heading, '*');
+  
 };
--- sandbox.js
+++ sandbox.js
@@ -1,68 +1,73 @@
 
-       var map;
-       var planeMarker;
-     
-      google.load("maps", "3", {other_params:'sensor=TRUEorFALSE',
-      
-      callback: function initialize() 
-	{
-	    var mapCanvas = document.getElementById('map-canvas');
-	    var mapOptions = 
-	    {
-	      center: new google.maps.LatLng(44.5403, -78.5463),
-	      zoom: 8,
-	      mapTypeId: google.maps.MapTypeId.ROADMAP
-	    }
-	  
-	  map = new google.maps.Map(mapCanvas, mapOptions);
-	  
-	  var myCenter=new google.maps.LatLng(51.508742,-0.120850);
-	  
-	  planeMarker=new google.maps.Marker({position:myCenter,  icon: 
-	  {
-	    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-	    strokeWeight:2,
-	    scale: 7,
-	    rotation: 0
-	  }
-	  ,});
+var map;
+var planeMarker;
+var tracking = true;
+var trailing = true;
+
+google.load("maps", "3", {other_params:'sensor=TRUEorFALSE',
   
-	  planeMarker.setMap(map);
-       
-	}
-      }
-    );
-       
-      var counter = 0;
-      function setMapCoords(lattitude, longitude, heading)
-      {
-	var newLatLng = new google.maps.LatLng(lattitude, longitude)
+  callback: function initialize() 
+  {
+    
+    var myCenter=new google.maps.LatLng(51.508742,-0.120850);
 	
-	  map.panTo(newLatLng);
-	
-	planeMarker.setPosition(newLatLng);
-	planeMarker.setIcon( {
-	    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-		strokeWeight:2,
-	    scale: 7,
-	    rotation: heading
-	  }
-	  );
-	  
-      }
-      
-      window.addEventListener("message", receiveMessage, false);
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = 
+    {
+      center: myCenter,
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    }
+    
+    map = new google.maps.Map(mapCanvas, mapOptions);
+    
 
-      function receiveMessage(event)
+    
+    planeMarker=new google.maps.Marker({position:myCenter,  icon: 
       {
-	//console.log(event.data);
-	
-	var coordsArray = event.data.split(",");
-	
-//	console.log(coordsArray);
-	
-	setMapCoords(parseFloat(coordsArray[0]), parseFloat(coordsArray[1]), parseFloat(coordsArray[2]));
-	
-
+	path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+	strokeWeight:3,
+	scale: 7,
+	rotation: 0
       }
-      
+      ,});
+    
+    planeMarker.setMap(map);
+    
+  }
+}
+);
+
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event)
+{
+  //console.log(event.data);
+  
+  var coordsArray = event.data.split(",");
+  
+  //	console.log(coordsArray);
+  
+  setMapCoords(parseFloat(coordsArray[0]), parseFloat(coordsArray[1]), parseFloat(coordsArray[2]));
+  
+  
+}
+
+function setMapCoords(lattitude, longitude, heading)
+{
+  var newLatLng = new google.maps.LatLng(lattitude, longitude)
+  
+  if(tracking)
+    map.panTo(newLatLng);
+  
+  planeMarker.setPosition(newLatLng);
+  planeMarker.setIcon( {
+    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+    strokeWeight:2,
+    scale: 7,
+    rotation: heading
+  }
+  );
+  
+}
