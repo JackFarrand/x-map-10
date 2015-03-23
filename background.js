395a028b9f8e03a958f0311892b180e907aac0c7
Jack Farrand <jack.farrand@gmail.com>
2015-03-23 16:29:05 +0000

    cleaned up background.js so the code is readable

--- background.js
+++ background.js
@@ -1,87 +1,77 @@
-var theId;
+var mainWindowID;			//ID variable to retrieve the main window created by the app
+var socketId;				//ID of the socket we are using to receive the UDP data
+var lat, lon, heading;			//Storage for lattitude, longitude, and headinging of the aircraft.
+
 
 chrome.app.runtime.onLaunched.addListener(
-  
-  function() 
-  {
-    chrome.app.window.create('window.html', 
-  {
-    id: "mainwin",
-    	innerBounds: {width: 500, height: 309}
-    
-  }, 
-  
-  function(win) //callback
-  {
-  theId = win.id;
-  }
-    );   
-});
+											function() 
+											{
+													  chrome.app.window.create('window.html', 
+													  {
+														id: "mainwin",
+														innerBounds: {width: 500, height: 309}
+													  }, 
+													  
+													  function(win) //callback
+													  {
+													  mainWindowID = win.id;
+													  }
+																			);   
+											});
 
-var socketId;
-
-var lat, lon, head;
-
-// Handle the "onReceive" event.
-var onReceive = function(info) 
-{
-  if (info.socketId !== socketId)
-    return;
-  
- //console.log(ab2str(info.data)); //convert byte array to text.  it is however, primarily floating point numbers, so get a better converter.
- //console.log(info.data.byteLength);  console.log("yup");
-  
- 
-
-var recLen = info.data.byteLength;
-
-recLen -= 36; //subtract the byte length of a data set transmission from XP10 from the Bytes received, to make sure we don't indexOutOfBounds
-
-//console.log("received bytes length: " + recLen)
-
-  for(var i = 5; i <= recLen; i+=36){
-    //first 4 bytes are the integer that identifies the next 8 floats.
-    var id = new Uint32Array(info.data.slice(i, i+4))[0]; 
-    //console.log("identifier received: " + id);
-    
-    if(id === 20)
-    {     
-       var floatView = new Float32Array(info.data.slice(i+4, i+36));    
-     //  console.log("Lattitude: " + floatView[0]);
-    //   console.log("Longitude: " + floatView[1]);
-       
-       lat = floatView[0];
-       lon = floatView[1];
-    }
-    
-    if(id === 17)
-    {     
-       var floatView = new Float32Array(info.data.slice(i+4, i+36));    
-     //  console.log("Lattitude: " + floatView[0]);
-    //   console.log("Longitude: " + floatView[1]);
-       
-       head = floatView[2];
-       
-    }
-  }
-
-  if(chrome.app.window.get("mainwin")) //if the damn window exists, write a message to it, else, just return.
-  chrome.app.window.get("mainwin").contentWindow.document.getElementById("mapFrame").contentWindow.postMessage("" + lat + "," + lon + "," + head, '*');
-  
-};
 
 // Create the Socket
-chrome.sockets.udp.create({}, function(socketInfo) {
-  socketId = socketInfo.socketId;
-  // Setup event handler and bind socket.
-  chrome.sockets.udp.onReceive.addListener(onReceive);
-  chrome.sockets.udp.bind(socketId,
-    "0.0.0.0", 49003, function(result) {
-      if (result < 0) {
-        console.log("Error binding socket.");
-        return;
-      }
-      else
-	console.log("socket bound!");     
-  });
-});
+chrome.sockets.udp.create({}, 	function(socketInfo) 
+								{
+									socketId = socketInfo.socketId;
+										  
+									// Setup event handler and bind socket.
+									chrome.sockets.udp.onReceive.addListener(onReceive);
+									chrome.sockets.udp.bind(socketId, "0.0.0.0", 49003, function(result) 
+																						{
+																							if (result < 0) 
+																							{
+																								console.log("Error binding socket.");
+																								return;
+																							}
+																							else
+																								console.log("socket bound!");     
+																						}
+															);
+								}
+						);
+
+var onReceive = function(info) // Handle the Socket "onReceive" event.
+{
+	if (info.socketId !== socketId)
+		return;
+	  
+	var recLen = info.data.byteLength;
+
+	recLen -= 36; //subtract the byte length of a data set transmission from XP10 from the Bytes received, to make sure we don't indexOutOfBounds
+
+	for(var i = 5; i <= recLen; i+=36)
+	{
+		//first 4 bytes are the integer that identifies the next 8 floats.
+		var id = new Uint32Array(info.data.slice(i, i+4))[0];  //console.log("identifier received: " + id);
+			
+		if(id === 20) //ID for Lattitude and Longitude Etc.
+		{     
+			var floatView = new Float32Array(info.data.slice(i+4, i+36));    
+		
+			lat = floatView[0];
+			lon = floatView[1];
+		}
+		
+		if(id === 17)
+		{     
+		   var floatView = new Float32Array(info.data.slice(i+4, i+36));    
+		
+		   heading = floatView[2];
+		}
+	}
+
+	if(chrome.app.window.get("mainwin")) //if the damn window exists, write a message to it, else, just return.
+		chrome.app.window.get("mainwin").contentWindow.document.getElementById("mapFrame").contentWindow.postMessage("" + lat + "," + lon + "," + heading, '*');
+	  
+};
